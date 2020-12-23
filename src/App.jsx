6cf47e03c7efd6b9f7801cd78aca26508a32ff1a
a.rubashka <a.rubashka@godeltech.com>
2020-12-23 17:03:18 +0300

    Little buttons are components and CSS were fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getData } from './helper'
 import './App.css'
 import FavIcon from './heart.svg'
+import ReloadIcon from './reload.svg'
 
 const Input = (props) => {
   const { value, setValue, error } = props
@@ -19,48 +20,49 @@ const Input = (props) => {
   )
 }
 
+const Button = (props) => {
+  return (
+    <button id="button_style" onClick={props.onClick}>
+      <img src={props.icon} alt={props.alt} />
+    </button>
+  )
+}
+
 const Result = (props) => {
   const { weather, addToFavs, onReloadClick } = props
+  const {
+    name,
+    temp,
+    description,
+    feels_like,
+    pressure,
+    humidity,
+    icon,
+  } = weather
+
   return (
     <div className="result">
       <span>
-        <span id="city_name">{weather.name} </span>
-        <span id="city_weather">{Math.round(weather.temp) - 273} ℃</span>
+        <span id="city_name">{name} </span>
+        <span id="city_weather">{Math.round(temp) - 273} ℃</span>
       </span>
 
-      <span className="description">{weather.description}</span>
+      <span className="description">{description}</span>
       <span className="icon">
         {' '}
         <img
-          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
           alt="Картинка погоды"
         />{' '}
       </span>
       <span className="feels_like">
-        Ощущается как {Math.round(weather.feels_like) - 273} ℃
+        Ощущается как {Math.round(feels_like) - 273} ℃
       </span>
-      <span className="pressure">
-        Атмосферное давление: {weather.pressure} гПа
-      </span>
-      <span className="humidity">
-        Относительная влажность: {weather.humidity} %
-      </span>
+      <span className="pressure">Атмосферное давление: {pressure} гПа</span>
+      <span className="humidity">Относительная влажность: {humidity} %</span>
       <div className="buttons">
-        <button id="reload" onClick={onReloadClick}>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-          >
-            <path d="M7 9h-7v-7h1v5.2c1.853-4.237 6.083-7.2 11-7.2 6.623 0 12 5.377 12 12s-5.377 12-12 12c-6.286 0-11.45-4.844-11.959-11h1.004c.506 5.603 5.221 10 10.955 10 6.071 0 11-4.929 11-11s-4.929-11-11-11c-4.66 0-8.647 2.904-10.249 7h5.249v1z" />
-          </svg>
-        </button>
-
-        <button id="favorite" onClick={addToFavs}>
-          <img src={FavIcon} alt="Сердуха" />
-        </button>
+        <Button onClick={addToFavs} icon={FavIcon} alt={'Favorite'} />
+        <Button onClick={onReloadClick} icon={ReloadIcon} alt={'Back'} />
       </div>
     </div>
   )
