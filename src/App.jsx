039d68ce5921e7a76e5f7cfcc8ef6ab92bb95c13
a.rubashka <a.rubashka@godeltech.com>
2020-12-23 15:55:10 +0300

    result component

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,53 @@ const Input = (props) => {
   )
 }
 
+const Result = (props) => {
+  const { weather, addToFavs, onReloadClick } = props
+  return (
+    <div className="result">
+      <span>
+        <span id="city_name">{weather.name} </span>
+        <span id="city_weather">{Math.round(weather.temp) - 273} ℃</span>
+      </span>
+
+      <span className="description">{weather.description}</span>
+      <span className="icon">
+        {' '}
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+          alt="Картинка погоды"
+        />{' '}
+      </span>
+      <span className="feels_like">
+        Ощущается как {Math.round(weather.feels_like) - 273} ℃
+      </span>
+      <span className="pressure">
+        Атмосферное давление: {weather.pressure} гПа
+      </span>
+      <span className="humidity">
+        Относительная влажность: {weather.humidity} %
+      </span>
+      <div className="buttons">
+        <button id="reload" onClick={onReloadClick}>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          >
+            <path d="M7 9h-7v-7h1v5.2c1.853-4.237 6.083-7.2 11-7.2 6.623 0 12 5.377 12 12s-5.377 12-12 12c-6.286 0-11.45-4.844-11.959-11h1.004c.506 5.603 5.221 10 10.955 10 6.071 0 11-4.929 11-11s-4.929-11-11-11c-4.66 0-8.647 2.904-10.249 7h5.249v1z" />
+          </svg>
+        </button>
+
+        <button id="favorite" onClick={addToFavs}>
+          <img src={FavIcon} alt="Сердуха" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   const [searchInput, setSearchInput] = useState('')
   const [weather, setWeather] = useState(null)
@@ -87,47 +134,11 @@ const App = () => {
           </button>
         </div>
       ) : (
-        <div className="result">
-          <span>
-            <span id="city_name">{weather.name} </span>
-            <span id="city_weather">{Math.round(weather.temp) - 273} ℃</span>
-          </span>
-
-          <span className="description">{weather.description}</span>
-          <span className="icon">
-            {' '}
-            <img
-              src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
-              alt="Картинка погоды"
-            />{' '}
-          </span>
-          <span className="feels_like">
-            Ощущается как {Math.round(weather.feels_like) - 273} ℃
-          </span>
-          <span className="pressure">
-            Атмосферное давление: {weather.pressure} гПа
-          </span>
-          <span className="humidity">
-            Относительная влажность: {weather.humidity} %
-          </span>
-          <div className="buttons">
-            <button id="reload" onClick={onReloadClick}>
-              <svg
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              >
-                <path d="M7 9h-7v-7h1v5.2c1.853-4.237 6.083-7.2 11-7.2 6.623 0 12 5.377 12 12s-5.377 12-12 12c-6.286 0-11.45-4.844-11.959-11h1.004c.506 5.603 5.221 10 10.955 10 6.071 0 11-4.929 11-11s-4.929-11-11-11c-4.66 0-8.647 2.904-10.249 7h5.249v1z" />
-              </svg>
-            </button>
-
-            <button id="favorite" onClick={addToFavs}>
-              <img src={FavIcon} alt="Сердуха" />
-            </button>
-          </div>
-        </div>
+        <Result
+          weather={weather}
+          addToFavs={addToFavs}
+          onReloadClick={onReloadClick}
+        />
       )}
     </div>
   )
